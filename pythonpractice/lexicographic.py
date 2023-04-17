c1caba4c98d82ef2d0f9a08a41d34f9dca4c3fe6
anshumann548 <anshumann548@gmail.com>
2023-04-17 12:04:34 +0530

    sorting the string in lexicographic order

---
+++ pythonpractice/lexicographic.py
@@ -0,0 +1,11 @@
+string = []
+x= int((input("Enter the no. of strings to be sorted : "))) 
+for i in range(0,x):
+    string.append(str(input()))
+for i in range(0,string.__len__(),1):
+    for j in range(0,string.__len__()-1,1):
+        if(string[j]>string[j+1]):
+            temp =string[j]
+            string[j] =string[j+1] 
+            string[j+1]=temp
+print(string)
