1aed5c1f32d7ea1abb80d1ab24a87ea374f3686a
anshumann548 <anshumann548@gmail.com>
2023-04-17 13:22:10 +0530

    sorting the string in lexicographic order & reversing the order

--- pythonpractice/lexicographic.py
+++ pythonpractice/lexicographic.py
@@ -1,4 +1,5 @@
 string = []
+reverse =[]
 x= int((input("Enter the no. of strings to be sorted : "))) 
 for i in range(0,x):
     string.append(str(input()))
@@ -8,4 +9,8 @@ for i in range(0,string.__len__(),1):
             temp =string[j]
             string[j] =string[j+1] 
             string[j+1]=temp
-print(string)
+print("Alphabetically sorted list is : ",string)
+
+for start in range(string.__len__()-1,-1,-1):
+    reverse.append(string[start])
+print(reverse)
