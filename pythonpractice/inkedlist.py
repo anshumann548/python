1c1f0e24ef3f9e6589ded759970327b177ed68ec
anshumann548 <anshumann548@gmail.com>
2023-04-20 18:02:57 +0530

    Changes

--- pythonpractice/inkedlist.py
+++ pythonpractice/inkedlist.py
@@ -1,22 +1,52 @@
+# class Node:
+#     def __init__(self,val):
+#         self.val=val
+#         self.next=None
+# Start=None
+# itr = None
+# var=0
+# while(var !=-2):
+#     var = int(input("Enter your values : "))
+#     x=Node(var)
+#     if Start is None:
+#         Start=x
+#         itr=Start
+#     else:
+#         itr.next=x
+#         itr=x
+
+# print("Nodes are")
+# while(hasattr(Start, 'val')):
+    
+#         print(Start.val,"=>", end="")
+#         Start=Start.next
+# else:print("End of list")
+
+
+
+
 class Node:
     def __init__(self,val):
-        self.val=val 
-        self.nextadr=None
-
-x=0
+        self.val=val
+        self.next=None
+Start=None
+itr = None
 var=0
-Start = None
-while(var!=-2):
-    var = int(input("Enter values you want"))
-    x = Node(var)
-    if Start is None: 
-        Start = x
-    
-    itr=Start
-    
-    while(itr.nextadr!=None):
-        if (Start != x):
-            itr=x
+while(var !=-2):
+    var = int(input("Enter your values : "))
+    x=Node(var)
+    if Start is None:
+        Start=x
+        itr=Start
+    else:
+        itr.next=x
+        itr=x
 
-print("Your list is ")
-while (Start!=None): print(Start.val,"->");Start=Start.nextadr
+print("Nodes are")
+while(Start!=None):
+        print(Start.val,"=>", end="")
+        Start=Start.next
+else:print("End of list")
+
+
+
